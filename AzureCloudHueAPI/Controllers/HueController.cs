6fa13db893bc67c10675408028583262e4290009
DieselWeazel <jonathan.harr92@gmail.com>
2022-03-30 10:15:05 +0200

    Initial commit

---
+++ AzureCloudHueAPI/Controllers/HueController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AzureCloudHue.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HueController : ControllerBase
+    {
+
+        private readonly ILogger<HueController> _logger;
+
+        public HueController(ILogger<HueController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello from Azure Cloud Hue";
+        }
+    }
+}
